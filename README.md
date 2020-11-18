30fe44740382f04fafe21fa05530f14ab3fca89d
Julie Altman <jtskolnik@gmail.com>
2020-11-18 10:52:14 -0500

    removed unnecessary comments, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# quincantine-
+# quincantine- 
+
+![icon for license](https://camo.githubusercontent.com/3ccf4c50a1576b0dd30b286717451fa56b783512/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c6963656e73652d4d49542d79656c6c6f772e737667)
+
+# Table of contents
+1. [Description](#Description)
+2. [Installation instructions](#Installation-instructions)
+3. [Usage](#usage)
+4. [Contributions](#Contributions)
+5. [Testing instructions](#Testing-instructions)
+6. [License](#License)
+7. [Questions](#Questions)
+
+## Description: 
+This is an application where the user can enter notes to themselves that are stored on a server, and then they can view those notes at a later time. The user can also delete notes once they are finished with them. 
+
+## Images of deployed application
+This application is deployed in Heroku. 
+
+When the user arrives he or she is greeted with the following screen:
+![welcome screen](./images/welcome_screen.JPG)
+
+Then the user can write a note as follows:
+![writing note](./images/writing_note.JPG)
+
+They can save the note when they are finished:
+![saving note](./images/saving_note.JPG)
+
+When they are finished with a note they can delete it:
+![deleting note before](./images/before_delete.JPG)
+![deleting note after](./images/after_delete.JPG)
+
+## Installation instructions: 
+This uses express, path, fs, and util.
+
+## Usage: 
+This project is geared towards people with jobs that require for them to remember a lot of small tasks and small details about that task, for example teachers.
+
+## Contributions: 
+This is currently a solo project for skolnikskolnik.
+
+## Testing instructions: 
+Users can run the application locally by entering "node script.js". Users should NOT use nodemon for this particular project.
+
+## License:  
+This project is covered under the MIT License license.
+
+## Questions
+Visit my <a href='https://www.github.com/skolnikskolnik'>github page.</a> 
+
+Email me at jtskolnik@gmail.com with any additional questions you may have. I am always happy to talk to users of my product!
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -31,64 +31,63 @@ app.use(express.static("public"));
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // Displays all notes as json
-app.get("/api/notes", function(req, res) {
-    res.json(dbNotes);
-  });
+app.get("/api/notes", function (req, res) {
+  res.json(dbNotes);
+});
 
 
 //Creates a path for post requesting new notes
 //Why is this happening twice?
-app.post("/api/notes", function(req, res) {
-    //Console logging newNote shows ONE object 
-    let newNote = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newNote
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    let id = newNote.title.replace(/\s+/g, "").toLowerCase();
-    
-    newNote.id = id;
+app.post("/api/notes", function (req, res) {
+  //Console logging newNote shows ONE object 
+  let newNote = req.body;
 
-    dbNotes.push(newNote);
-    console.log("this is the first test");
-    
-    //Why is this happening twice?? It is pushing twice when the post request occurs client-side but when done on postman it works
-    writeFileAsync('./db/db.json', JSON.stringify(dbNotes)).then(function(){
-        console.log("test bby");
-        res.json(newNote);
-    }).catch(err => {
-        console.log(err);
-    });
+  // Using a RegEx Pattern to remove spaces from newNote
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  let id = newNote.title.replace(/\s+/g, "").toLowerCase();
+
+  newNote.id = id;
+
+  dbNotes.push(newNote);
+
+
+  //Why is this happening twice?? It is pushing twice when the post request occurs client-side but when done on postman it works
+  writeFileAsync('./db/db.json', JSON.stringify(dbNotes)).then(function () {
+    res.json(newNote);
+  }).catch(err => {
+    console.log(err);
   });
+});
 
 //The :id helps it look for the delete route
-  app.delete("/api/notes/:id", function(req, res) {
-    let notes = dbNotes.filter(note => note.id !== req.params.id);
-  
-    dbNotes = notes;
+app.delete("/api/notes/:id", function (req, res) {
+  let notes = dbNotes.filter(note => note.id !== req.params.id);
 
-    //dbNotes.push(notes);
+  dbNotes = notes;
 
-    //This writes to the json file and ensures that the display doesn't begin until the file is written
-    //Since db.json contains an array, dbNotes must be stringified
-    writeFileAsync('./db/db.json', JSON.stringify(dbNotes)).then(function(){
-        res.json(dbNotes);
-    }).catch(err => {
-        console.log(err);
-    });
-  
+  //dbNotes.push(notes);
+
+  //This writes to the json file and ensures that the display doesn't begin until the file is written
+  //Since db.json contains an array, dbNotes must be stringified
+  writeFileAsync('./db/db.json', JSON.stringify(dbNotes)).then(function () {
+    res.json(dbNotes);
+  }).catch(err => {
+    console.log(err);
   });
+
+});
 
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
